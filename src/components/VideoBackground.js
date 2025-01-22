c491608e5357e82238aaf75420c3018463848e8f
Nadif Hasan Purno <nadifhasanpurno@gmail.com>
2025-01-22 19:33:05 +0600

    made the trailer key dynamic

---
+++ src/components/VideoBackground.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { API_OPTIONS } from "../utils/constants";
+
+const VideoBackground = ({ movieId }) => {
+
+
+    const [trailerId, setTrailerId] = useState(null);
+
+
+
+
+  const getMovieVideos = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/539972/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    console.log(json);
+
+    const filterData = json.results.filter((video) => video.type === "Trailer");
+    const trailer = filterData.length ? filterData[0]: json.results[0];
+
+    console.log(trailer);
+    setTrailerId(trailer?.key);
+  };
+
+  useEffect(() => {
+    getMovieVideos();
+  }, []);
+
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src={"https://www.youtube.com/embed/" + trailerId}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      ></iframe>
+    </div>
+  );
+};
+
+export default VideoBackground;
